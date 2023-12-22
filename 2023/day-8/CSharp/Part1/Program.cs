bf24bc6edb06eb757dcb0328c6a74daf980f6c36
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2023-12-22 19:04:29 +0100

    2023 day 8 part 1 (does not work)

---
+++ 2023/day-8/CSharp/Part1/Program.cs
@@ -0,0 +1,115 @@
+﻿using Sprache;
+
+namespace Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../../../input.txt";
+            string input = File.ReadAllText(path);
+            DirectionInformation directionInformation = InputParser.ParseInput(input);
+            Dictionary<(string, Direction), string> locationDict = GetLocationDictionary(directionInformation.Nodes);
+            string start = directionInformation.Nodes.First().Item1;
+            string end = directionInformation.Nodes.Last().Item1;
+
+            string current = start;
+            long steps = 0;
+            foreach (Direction direction in GetInfiniteDirectionIterator(directionInformation.Directions))
+            {
+                if (current == end)
+                {
+                    break;
+                }
+
+                steps++;
+
+                if (steps % 100_000 == 0)
+                {
+                    Console.WriteLine($"Steps: {steps}");
+                }
+
+                current = locationDict[(current, direction)];
+            }
+
+            Console.WriteLine(steps);
+        }
+
+        public static IEnumerable<Direction> GetInfiniteDirectionIterator(List<Direction> directions)
+        {
+            while (true)
+            {
+                foreach (Direction direction in directions)
+                {
+                    yield return direction;
+                }
+            }
+        }
+
+        public static Dictionary<(string, Direction), string> GetLocationDictionary(List<(string, string, string)> nodes)
+        {
+            Dictionary<(string, Direction), string> result = new Dictionary<(string, Direction), string>();
+            foreach (var node in nodes)
+            {
+                result[(node.Item1, Direction.Left)] = node.Item2;
+                result[(node.Item1, Direction.Right)] = node.Item3;
+            }
+
+            return result;
+        }
+    }
+
+    public enum Direction
+    {
+        Left,
+        Right
+    }
+
+    public class DirectionInformation
+    {
+        public List<Direction> Directions;
+        public List<(string, string, string)> Nodes;
+
+        public DirectionInformation(List<Direction> directions, List<(string, string, string)> nodes)
+        {
+            Directions = directions;
+            Nodes = nodes;
+        }
+    }
+
+    public static class InputParser
+    {
+        public static DirectionInformation ParseInput(string input)
+        {
+            return parseDirectionInformation.Parse(input);
+        }
+
+        private static Direction CharToDirection(char c)
+        {
+            switch (c)
+            {
+                case 'L':
+                    return Direction.Left;
+                case 'R':
+                    return Direction.Right;
+                default:
+                    throw new Exception($"The char '{c}' does not match a direction");
+            }
+        }
+
+        private static readonly Parser<(string, string, string)> parseNode =
+            from nodeName in Parse.Letter.AtLeastOnce().Text()
+            from str in Parse.String(" = (")
+            from left in Parse.Letter.AtLeastOnce().Text()
+            from str2 in Parse.String(", ")
+            from right in Parse.Letter.AtLeastOnce().Text()
+            from str3 in Parse.String(")")
+            select (nodeName, left, right);
+
+        private static readonly Parser<DirectionInformation> parseDirectionInformation =
+            from directions in Parse.Char('L').Or(Parse.Char('R')).AtLeastOnce()
+            from newLines in Parse.String("\n\n")
+            from nodes in parseNode.DelimitedBy(Parse.Char('\n'))
+            select new DirectionInformation(directions.Select(CharToDirection).ToList(), nodes.ToList());
+    }
+}
