5e01b743d39c836f080da4a8bb8a56de342469ca
Laptop\danio <daniogdan@gmail.com>
2023-12-23 02:24:56 +0100

    2023 day 9

---
+++ 2023/day-9/CSharp/Part2/Program.cs
@@ -0,0 +1,84 @@
+﻿using Sprache;
+
+namespace Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../../../input.txt";
+            string input = File.ReadAllText(path);
+            List<OasisHistory> histories = InputParser.ParseInput(input);
+
+            long result = histories.Select(x => Extrapolate(x.History)).Sum();
+        }
+
+        private static long Extrapolate(IEnumerable<long> list)
+        {
+            List<long> firstItems = new List<long>();
+
+            IEnumerable<long> differences = list;
+            while (true)
+            {
+                firstItems.Add(differences.First());
+                differences = differences.GetPairs().Select(x => x.Item2 - x.Item1);
+
+                if (differences.AllAreEqual(0))
+                {
+                    break;
+                }
+            }
+
+            long currentNum = 0;
+            foreach (long item in firstItems.Reverse<long>())
+            {
+                currentNum = item - currentNum;
+            }
+
+            return currentNum;
+        }
+    }
+
+    public class OasisHistory
+    {
+        public IEnumerable<long> History;
+
+        public OasisHistory(IEnumerable<long> history)
+        {
+            History = history;
+        }
+    }
+
+    public static class InputParser
+    {
+        public static List<OasisHistory> ParseInput(string input)
+        {
+            return historyListParser.Parse(input);
+        }
+
+        private static readonly Parser<OasisHistory> historyParser =
+            from numbers in (
+                    from minus in Parse.Char('-').Optional()
+                    from number in Parse.Digit.AtLeastOnce().Text()
+                    select minus.IsDefined ? long.Parse("-" + number) : long.Parse(number)
+                ).DelimitedBy(Parse.Char(' '))
+            select new OasisHistory(numbers);
+
+        private static readonly Parser<List<OasisHistory>> historyListParser =
+            from histories in historyParser.DelimitedBy(Parse.Char('\n'))
+            select histories.ToList();
+    }
+
+    public static class LinqExtension
+    {
+        public static IEnumerable<(T, T)> GetPairs<T>(this IEnumerable<T> source)
+        {
+            return source.SkipLast(1).Zip(source.Skip(1), (a, b) => (a, b));
+        }
+
+        public static bool AllAreEqual<T>(this IEnumerable<T> source, T item) where T : IComparable<T>
+        {
+            return source.All(element => element.Equals(item));
+        }
+    }
+}
