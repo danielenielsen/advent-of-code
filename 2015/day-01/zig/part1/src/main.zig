80476dcb70f762c64a8ba247f5b2c77f9c5001f9
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2024-10-20 15:43:08 +0200

    2015 day 1 part 1 zig

--- 2015/day-01/zig/part1/src/main.zig
+++ 2015/day-01/zig/part1/src/main.zig
@@ -1,6 +1,30 @@
 const std = @import("std");
 
 pub fn main() !void {
-    
+    const file = try std.fs.cwd().openFile("../../input.txt", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    var buf: [10000]u8 = undefined;
+    const bytes_read = try in_stream.readAll(&buf);
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Read {d} bytes\n", .{ bytes_read });
+
+    const string = buf[0..bytes_read];
+    // try stdout.print("{s}", .{ string });
+
+    var level: i32 = 0;
+    for (string) |char| {
+        if (char == '(') {
+            level += 1;
+        } else {{
+            level -= 1;
+        }}
+    }
+
+    try stdout.print("The level is {d}\n", .{ level });
 }
 
