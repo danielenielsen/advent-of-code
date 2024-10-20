7827e51321d4f3e7b0e0b34c2fe82eec9106666b
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2024-10-20 17:25:07 +0200

    2015 day 2 part 2 zig

---
+++ 2015/day-02/zig/part2/src/main.zig
@@ -0,0 +1,76 @@
+const std = @import("std");
+const mecha = @import("mecha");
+
+const LineData = struct {
+    length: i32,
+    width: i32,
+    height: i32,
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const input = try readFile("../../input.txt", allocator);
+    defer allocator.free(input);
+
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Input has a length of {d}\n", .{ input.len });
+
+    const data = (try file_parser.parse(allocator, input)).value;
+
+    var sum: i32 = 0;
+    for (data) |linedata| { 
+        sum += handleLine(&linedata);
+    }
+
+    try stdout.print("SUM: {d}\n", .{ sum });
+    try bw.flush();
+}
+
+fn readFile(file_name: []const u8, allocator: std.mem.Allocator) ![]u8 {
+    const file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+
+    const file_stats = try file.stat();
+    const file_size = file_stats.size;
+    const buffer = try allocator.alloc(u8, file_size);
+    _ = try file.readAll(buffer);
+    return buffer;
+}
+
+fn handleLine(linedata: *const LineData) i32 {
+    const ribbon_wrap: i32 = @min(
+        2 * linedata.length + 2 * linedata.width,
+        2 * linedata.length + 2 * linedata.height,
+        2 * linedata.width + 2 * linedata.height,
+    );
+    const ribbon_bow: i32 = linedata.length * linedata.width * linedata.height;
+
+    return ribbon_wrap + ribbon_bow;
+}
+
+const file_parser = mecha.many(
+    line_parser,
+    .{
+        .separator = newline_parser
+    }
+);
+
+const line_parser = mecha.map(
+    mecha.combine(.{
+        mecha.int(i32, .{ .base = 10 }),
+        mecha.ascii.char('x').discard(),
+        mecha.int(i32, .{ .base = 10 }),
+        mecha.ascii.char('x').discard(),
+        mecha.int(i32, .{ .base = 10 }),
+    }),
+    mecha.toStruct(LineData)
+);
+
+const newline_parser = mecha.oneOf(.{
+    mecha.string("\r\n").discard(),
+    mecha.ascii.char('\n').discard(),
+});
