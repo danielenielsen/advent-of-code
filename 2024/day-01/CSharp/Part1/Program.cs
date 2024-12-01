3ceab292e7a8ba2688375f6076e7cbc366ab533e
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2024-12-01 06:33:43 +0100

    2024 c# day 1

--- 2024/day-01/CSharp/Part1/Program.cs
+++ 2024/day-01/CSharp/Part1/Program.cs
@@ -7,7 +7,36 @@
             string path = "../../../../../input.txt";
             string input = File.ReadAllText(path);
 
-            Console.WriteLine(input);
+            (List<int> left, List<int> right) = GetLeftAndRightLists(input);
+
+            int result = left.OrderBy(x => x).Zip(right.OrderBy(x => x), (x, y) => Math.Abs(x - y)).Sum();
+            Console.WriteLine(result);
+        }
+
+        private static (List<int>, List<int>) GetLeftAndRightLists(string input)
+        {
+            List<string> lines = input.Split("\n").Where(x => x != "").ToList();
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            foreach (string line in lines)
+            {
+                List<string> split = line.Split("   ").ToList();
+
+                if (split.Count != 2)
+                {
+                    throw new Exception("The split had a length of " + split.Count);
+                }
+
+                int leftNumber = int.Parse(split[0]);
+                int rightNumber = int.Parse(split[1]);
+
+                left.Add(leftNumber);
+                right.Add(rightNumber);
+            }
+
+            return (left, right);
         }
     }
 }
--- 2024/day-01/CSharp/Part2/Program.cs
+++ 2024/day-01/CSharp/Part2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Part2
+﻿using System.Linq;
+
+namespace Part2
 {
     internal class Program
     {
@@ -7,7 +9,37 @@
             string path = "../../../../../input.txt";
             string input = File.ReadAllText(path);
 
-            Console.WriteLine(input);
+            (List<int> left, List<int> right) = GetLeftAndRightLists(input);
+
+            Dictionary<int, int> rightSimilarity = right.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            int result = left.Select(x => rightSimilarity.TryGetValue(x, out int similarity) ? x * similarity : 0).Sum();
+            Console.WriteLine(result);
+        }
+
+        private static (List<int>, List<int>) GetLeftAndRightLists(string input)
+        {
+            List<string> lines = input.Split("\n").Where(x => x != "").ToList();
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            foreach (string line in lines)
+            {
+                List<string> split = line.Split("   ").ToList();
+
+                if (split.Count != 2)
+                {
+                    throw new Exception("The split had a length of " + split.Count);
+                }
+
+                int leftNumber = int.Parse(split[0]);
+                int rightNumber = int.Parse(split[1]);
+
+                left.Add(leftNumber);
+                right.Add(rightNumber);
+            }
+
+            return (left, right);
         }
     }
 }
