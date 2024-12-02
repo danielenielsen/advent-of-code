0b7c8ee19b526dd7ef82a0126479f3019ca981ed
DALUX\dn <dn@dalux.com>
2024-12-02 07:31:39 +0100

    2024 day 2 C#

--- 2024/day-02/CSharp/Part1/Program.cs
+++ 2024/day-02/CSharp/Part1/Program.cs
@@ -7,7 +7,31 @@
             string path = "../../../../../input.txt";
             string input = File.ReadAllText(path);
 
-            Console.WriteLine(input);
+            List<string> lines = input.Split("\n").Where(x => x != "").ToList();
+
+            int count = 0;
+            foreach (string line in lines)
+            {
+                List<int> numbers = line.Split(" ").Select(int.Parse).ToList();
+                bool numbersAscending = numbers.SequenceEqual(numbers.OrderBy(x => x));
+                bool numbersDescending = numbers.SequenceEqual(numbers.OrderByDescending(x => x));
+
+                if (!numbersAscending && !numbersDescending)
+                {
+                    continue;
+                }
+
+                bool goodDiffs = numbers.SkipLast(1).Zip(numbers.Skip(1), (x, y) => Math.Abs(x - y)).All(x => x > 0 && x <= 3);
+
+                if (!goodDiffs)
+                {
+                    continue;
+                }
+
+                count++;
+            }
+
+            Console.WriteLine($"Result: {count}");
         }
     }
 }
--- 2024/day-02/CSharp/Part2/Program.cs
+++ 2024/day-02/CSharp/Part2/Program.cs
@@ -7,7 +7,53 @@
             string path = "../../../../../input.txt";
             string input = File.ReadAllText(path);
 
-            Console.WriteLine(input);
+            List<string> lines = input.Split("\n").Where(x => x != "").ToList();
+
+            int count = 0;
+            foreach (string line in lines)
+            {
+                List<int> numbers = line.Split(" ").Select(int.Parse).ToList();
+
+                if (ReportIsSafe(numbers))
+                {
+                    count++;
+                    continue;
+                }
+
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    List<int> copy = numbers.ToList();
+                    copy.RemoveAt(i);
+
+                    if (ReportIsSafe(copy))
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Result: {count}");
+        }
+
+        private static bool ReportIsSafe(List<int> numbers)
+        {
+            bool numbersAscending = numbers.SequenceEqual(numbers.OrderBy(x => x));
+            bool numbersDescending = numbers.SequenceEqual(numbers.OrderByDescending(x => x));
+
+            if (!numbersAscending && !numbersDescending)
+            {
+                return false;
+            }
+
+            bool goodDiffs = numbers.SkipLast(1).Zip(numbers.Skip(1), (x, y) => Math.Abs(x - y)).All(x => x > 0 && x <= 3);
+
+            if (!goodDiffs)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
