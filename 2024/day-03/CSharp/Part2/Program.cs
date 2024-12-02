954ada2e04064a58b2743d59366f280bba3b1ce5
DALUX\dn <dn@dalux.com>
2024-12-02 07:36:36 +0100

    Prepare empty C# for 2024 day 3

---
+++ 2024/day-03/CSharp/Part2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../../../input.txt";
+            string input = File.ReadAllText(path);
+
+            Console.WriteLine(input);
+        }
+    }
+}
