38923ba9afdb151975466478c7bee9097c279d69
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2024-12-02 19:23:22 +0100

    2023 day 15 C#

---
+++ 2023/day-15/CSharp/Part1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../../../input.txt";
+            string input = File.ReadAllText(path);
+
+            List<string> steps = input.Trim().Replace("\n", "").Split(',').ToList();
+
+            int result = 0;
+            foreach (string step in steps)
+            {
+                int hash = 0;
+                foreach (char c in step)
+                {
+                    hash += c;
+                    hash *= 17;
+                    hash %= 256;
+                }
+
+                result += hash;
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
