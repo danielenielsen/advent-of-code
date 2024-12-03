79a29ae3f661f1f4547867073829d035d19de25d
DALUX\dn <dn@dalux.com>
2024-12-03 07:39:35 +0100

    2024 day 3 C#

--- 2024/day-03/CSharp/Part1/Program.cs
+++ 2024/day-03/CSharp/Part1/Program.cs
@@ -5,9 +5,73 @@
         static void Main(string[] args)
         {
             string path = "../../../../../input.txt";
-            string input = File.ReadAllText(path);
+            string input = File.ReadAllText(path).Trim().Replace("\r", "").Replace("\n", "");
 
-            Console.WriteLine(input);
+            int result = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != 'm')
+                {
+                    continue;
+                }
+
+                if (i + 1 < input.Length && input[i + 1] != 'u')
+                {
+                    continue;
+                }
+
+                if (i + 2 < input.Length && input[i + 2] != 'l')
+                {
+                    continue;
+                }
+
+                if (i + 3 < input.Length && input[i + 3] != '(')
+                {
+                    continue;
+                }
+
+                int pos = input[(i + 4)..].IndexOf(')');
+
+                if (pos == -1)
+                {
+                    break;
+                }
+
+                if (pos < 3)
+                {
+                    continue;
+                }
+
+                List<string> split = input[(i + 4)..(i + 4 + pos)].Split(',').ToList();
+                if (split.Count != 2)
+                {
+                    continue;
+                }
+
+                if (split[0] != split[0].Trim())
+                {
+                    continue;
+                }
+
+                if (split[1] != split[1].Trim())
+                {
+                    continue;
+                }
+
+                if (!int.TryParse(split[0], out int x))
+                {
+                    continue;
+                }
+
+                if (!int.TryParse(split[1], out int y))
+                {
+                    continue;
+                }
+
+                result += x * y;
+            }
+
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
--- 2024/day-03/CSharp/Part2/Program.cs
+++ 2024/day-03/CSharp/Part2/Program.cs
@@ -5,9 +5,74 @@
         static void Main(string[] args)
         {
             string path = "../../../../../input.txt";
-            string input = File.ReadAllText(path);
+            string input = File.ReadAllText(path).Trim().Replace("\r", "").Replace("\n", "");
 
-            Console.WriteLine(input);
+            bool enabled = true;
+            int result = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i + 6 < input.Length && input[i..(i + 7)] == "don't()")
+                {
+                    enabled = false;
+                }
+
+                if (i + 3 < input.Length && input[i..(i + 4)] == "do()")
+                {
+                    enabled = true;
+                }
+
+                if (!enabled)
+                {
+                    continue;
+                }
+
+                if (i + 3 < input.Length && input[i..(i + 4)] != "mul(")
+                {
+                    continue;
+                }
+
+                int pos = input[(i + 4)..].IndexOf(')');
+
+                if (pos == -1)
+                {
+                    break;
+                }
+
+                if (pos < 3)
+                {
+                    continue;
+                }
+
+                List<string> split = input[(i + 4)..(i + 4 + pos)].Split(',').ToList();
+                if (split.Count != 2)
+                {
+                    continue;
+                }
+
+                if (split[0] != split[0].Trim())
+                {
+                    continue;
+                }
+
+                if (split[1] != split[1].Trim())
+                {
+                    continue;
+                }
+
+                if (!int.TryParse(split[0], out int x))
+                {
+                    continue;
+                }
+
+                if (!int.TryParse(split[1], out int y))
+                {
+                    continue;
+                }
+
+                result += x * y;
+            }
+
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
