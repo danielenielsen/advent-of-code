05f7ff851a2dfe48e409182ac26bd3b7c6164287
DALUX\dn <dn@dalux.com>
2024-12-05 08:05:51 +0100

    2024 day 6 prepare C#

---
+++ 2024/day-06/CSharp/Part1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../../../input.txt";
+            string input = File.ReadAllText(path).Replace("\r", "");
+
+            Console.WriteLine(input);
+        }
+    }
+}
