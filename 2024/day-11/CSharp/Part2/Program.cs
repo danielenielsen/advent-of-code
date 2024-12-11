ab8333dad9ba3c28e9762bfa80d1d296fef68400
DALUX\dn <dn@dalux.com>
2024-12-11 09:47:52 +0100

    2024 day 11 part 2

--- 2024/day-11/CSharp/Part2/Program.cs
+++ 2024/day-11/CSharp/Part2/Program.cs
@@ -8,53 +8,52 @@
             string input = File.ReadAllText(path).Replace("\r", "").Trim();
             List<Stone> stones = ParseInput(input);
 
-            for (int i = 0; i < 75; i++)
-            {
-                stones = Blink(stones);
-            }
+            Dictionary<(long stoneNum, int blink), long> stoneBlinkLookup = new Dictionary<(long stoneNum, int blink), long>();
 
-            int result = stones.Count;
+            long result = stones.Select(x => Blink(stoneBlinkLookup, x.Number, 75)).Sum();
             Console.WriteLine($"Result: {result}");
         }
 
-        static List<Stone> Blink(List<Stone> currentStones)
+        static long Blink(Dictionary<(long stoneNum, int blink), long> stoneBlinkLookup, long stoneNum, int blink)
         {
-            List<Stone> stones = currentStones.ToList();
-
-            for (int i = stones.Count - 1; i >= 0; i--)
+            if (blink == 0)
             {
-                if (stones[i].Number == 0)
-                {
-                    stones[i].Number += 1;
-                    continue;
-                }
-
-                if (stones[i].Number.ToString().Count() % 2 == 0)
-                {
-                    string num = stones[i].Number.ToString();
-                    int half = num.Length / 2;
-                    string first = num[..half];
-                    string second = num[half..];
-
-                    if (first.Contains("-") || second.Contains("-"))
-                    {
-                        throw new Exception("Not supposed to be below 0");
-                    }
-
-                    long firstNumber = long.Parse(first);
-                    long secondNumber = long.Parse(second);
-
-                    stones[i].Number = firstNumber;
-                    Stone newStone = new Stone(secondNumber);
-                    stones.Insert(i + 1, newStone);
-
-                    continue;
-                }
-
-                stones[i].Number *= 2024;
+                return 1;
             }
 
-            return stones;
+            if (stoneBlinkLookup.TryGetValue((stoneNum, blink), out long res))
+            {
+                return res;
+            }
+
+            if (stoneNum == 0)
+            {
+                res = Blink(stoneBlinkLookup, 1, blink - 1);
+            }
+            else if (stoneNum.ToString().Length % 2 == 0)
+            {
+                string num = stoneNum.ToString();
+                int half = num.Length / 2;
+                string first = num[..half];
+                string second = num[half..];
+
+                if (first.Contains("-") || second.Contains("-"))
+                {
+                    throw new Exception("Not supposed to be below 0");
+                }
+
+                long firstNumber = long.Parse(first);
+                long secondNumber = long.Parse(second);
+
+                res = Blink(stoneBlinkLookup, firstNumber, blink - 1) + Blink(stoneBlinkLookup, secondNumber, blink - 1);
+            }
+            else
+            {
+                res = Blink(stoneBlinkLookup, stoneNum * 2024, blink - 1);
+            }
+
+            stoneBlinkLookup[(stoneNum, blink)] = res;
+            return res;
         }
 
         static List<Stone> ParseInput(string input)
