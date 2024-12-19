c1b152ca3d71cb9563794398a0b36c06aa79da9f
DALUX\dn <dn@dalux.com>
2024-12-19 08:37:19 +0100

    2024 day 19 C#

--- 2024/day-19/CSharp/Part1/Program.cs
+++ 2024/day-19/CSharp/Part1/Program.cs
@@ -6,8 +6,52 @@
         {
             string path = "../../../../../input.txt";
             string input = File.ReadAllText(path).Replace("\r", "").Trim();
+            (List<string> availableTowels, List<string> patternsToMatch) = ParseInput(input);
 
-            Console.WriteLine(input);
+            Dictionary<string, bool> patternLookup = new Dictionary<string, bool>();
+            int matches = patternsToMatch.Count(x => IsMatch(patternLookup, availableTowels, x));
+            Console.WriteLine($"Matches: {matches}");
+        }
+
+        static (List<string> availableTowels, List<string> patternsToMatch) ParseInput(string input)
+        {
+            string[] parts = input.Split("\n\n");
+
+            List<string> availableTowels = parts[0].Split(", ").OrderByDescending(x=> x.Length).ToList();
+            List<string> patternsToMatch = parts[1].Split("\n").ToList();
+
+            return (availableTowels, patternsToMatch);
+        }
+
+        static bool IsMatch(Dictionary<string, bool> patternLookup, List<string> availableTowels, string pattern)
+        {
+            if (pattern.Length == 0)
+            {
+                return true;
+            }
+
+            if (patternLookup.TryGetValue(pattern, out bool res))
+            {
+                return res;
+            }
+
+            bool isMatch = false;
+            List<string> candidates = availableTowels.Where(pattern.StartsWith).ToList();
+            foreach (string towel in candidates)
+            {
+                string remainingPattern = pattern[towel.Length..];
+
+                if (!IsMatch(patternLookup, availableTowels, remainingPattern))
+                {
+                    continue;
+                }
+
+                isMatch = true;
+                break;
+            }
+
+            patternLookup.Add(pattern, isMatch);
+            return isMatch;
         }
     }
 }
--- 2024/day-19/CSharp/Part2/Program.cs
+++ 2024/day-19/CSharp/Part2/Program.cs
@@ -6,8 +6,46 @@
         {
             string path = "../../../../../input.txt";
             string input = File.ReadAllText(path).Replace("\r", "").Trim();
+            (List<string> availableTowels, List<string> patternsToMatch) = ParseInput(input);
 
-            Console.WriteLine(input);
+            Dictionary<string, long> patternLookup = new Dictionary<string, long>();
+            long matches = patternsToMatch.Sum(x => MatchCount(patternLookup, availableTowels, x));
+            Console.WriteLine($"Match count: {matches}");
+        }
+
+        static (List<string> availableTowels, List<string> patternsToMatch) ParseInput(string input)
+        {
+            string[] parts = input.Split("\n\n");
+
+            List<string> availableTowels = parts[0].Split(", ").OrderByDescending(x => x.Length).ToList();
+            List<string> patternsToMatch = parts[1].Split("\n").ToList();
+
+            return (availableTowels, patternsToMatch);
+        }
+
+        static long MatchCount(Dictionary<string, long> patternLookup, List<string> availableTowels, string pattern)
+        {
+            if (patternLookup.TryGetValue(pattern, out long res))
+            {
+                return res;
+            }
+
+            List<string> candidates = availableTowels.Where(pattern.StartsWith).ToList();
+            long count = 0;
+
+            foreach (string towel in candidates)
+            {
+                if (towel == pattern)
+                {
+                    count += 1;
+                }
+
+                string remainingPattern = pattern[towel.Length..];
+                count += MatchCount(patternLookup, availableTowels, remainingPattern);
+            }
+
+            patternLookup.Add(pattern, count);
+            return count;
         }
     }
 }
