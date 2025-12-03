ede7dae7e5e4e64cc911fdc06ab4d1573c53a878
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2025-12-03 19:10:35 +0100

    2025 day 3 C#

---
+++ 2025/day-03/CSharp/Part1/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Part1;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        string input = GetInput();
+        IEnumerable<Bank> banks = ParseInput(input);
+        int res = Solve(banks);
+        Console.WriteLine($"Result: {res}");
+    }
+
+    private static int Solve(IEnumerable<Bank> banks)
+    {
+        return banks.Select(x => x.GetMaxJoltage()).Sum();
+    }
+    
+    private static string GetInput()
+    {
+        return File.ReadAllText("../../../../../input.txt");
+    }
+
+    private static IEnumerable<Bank> ParseInput(string input)
+    {
+        string[] lines = input.Trim().Replace("\r", "").Split("\n");
+        List<Bank> res = [];
+
+        foreach (string line in lines)
+        {
+            List<Battery> batteries = [];
+
+            foreach (char c in line)
+            {
+                int joltage = int.Parse(c.ToString());
+                batteries.Add(new Battery(joltage));
+            }
+            
+            res.Add(new Bank(batteries));
+        }
+        
+        return res;
+    }
+}
+
+internal class Battery(int joltage)
+{
+    public int Joltage { get; } = CheckJoltage(joltage);
+
+    private static int CheckJoltage(int joltage)
+    {
+        if (joltage is < 1 or > 9)
+        {
+            throw new ArgumentOutOfRangeException(nameof(joltage));
+        }
+        
+        return joltage;
+    }
+}
+
+internal class Bank(List<Battery> batteries)
+{
+    public List<Battery> Batteries { get; } = batteries;
+
+    public int GetMaxJoltage()
+    {
+        int firstJoltage = 0;
+        int secondJoltage = 0;
+
+        for (int i = 0; i < Batteries.Count; i++)
+        {
+            Battery battery = Batteries[i];
+            
+            if (battery.Joltage > firstJoltage && i < Batteries.Count - 1)
+            {
+                firstJoltage = battery.Joltage;
+                secondJoltage = Batteries[i + 1].Joltage;
+                continue;
+            }
+
+            if (battery.Joltage > secondJoltage)
+            {
+                secondJoltage = battery.Joltage;
+            }
+        }
+        
+        return firstJoltage * 10 + secondJoltage;
+    }
+}
---
+++ 2025/day-03/CSharp/Part2/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Part2;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        string input = GetInput();
+        IEnumerable<Bank> banks = ParseInput(input);
+        long res = Solve(banks);
+        Console.WriteLine($"Result: {res}");
+    }
+
+    private static long Solve(IEnumerable<Bank> banks)
+    {
+        return banks.Select(x => x.GetMaxJoltage()).Sum();
+    }
+    
+    private static string GetInput()
+    {
+        return File.ReadAllText("../../../../../input.txt");
+    }
+
+    private static IEnumerable<Bank> ParseInput(string input)
+    {
+        string[] lines = input.Trim().Replace("\r", "").Split("\n");
+        List<Bank> res = [];
+
+        foreach (string line in lines)
+        {
+            List<Battery> batteries = [];
+
+            foreach (char c in line)
+            {
+                int joltage = int.Parse(c.ToString());
+                batteries.Add(new Battery(joltage));
+            }
+            
+            res.Add(new Bank(batteries));
+        }
+        
+        return res;
+    }
+}
+
+internal class Battery(int joltage)
+{
+    public int Joltage { get; } = CheckJoltage(joltage);
+
+    private static int CheckJoltage(int joltage)
+    {
+        if (joltage is < 1 or > 9)
+        {
+            throw new ArgumentOutOfRangeException(nameof(joltage));
+        }
+        
+        return joltage;
+    }
+}
+
+internal class Bank(List<Battery> batteries)
+{
+    public List<Battery> Batteries { get; } = batteries;
+
+    public long GetMaxJoltage()
+    {
+        int[] joltageIndexes = Enumerable.Repeat(-1, 12).ToArray();
+
+        for (int i = 0; i < joltageIndexes.Length; i++)
+        {
+            int previousIndex = i > 0 ? joltageIndexes[i - 1] : -1;
+            
+            for (int j = previousIndex + 1; j < Batteries.Count - (joltageIndexes.Length - i - 1); j++)
+            {
+                if (joltageIndexes[i] == -1)
+                {
+                    joltageIndexes[i] = j;
+                    continue;
+                }
+                
+                if (Batteries[j].Joltage > Batteries[joltageIndexes[i]].Joltage)
+                {
+                    joltageIndexes[i] = j;
+                }
+            }
+        }
+        
+        return joltageIndexes
+            .Select(x => Batteries[x])
+            .Reverse()
+            .Select((val, idx) => (long)Math.Pow(10, idx) * val.Joltage)
+            .Sum();
+    }
+}
